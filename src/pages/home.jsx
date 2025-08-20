7bf67bbbec0091fc9793fc549cef5d5fe329fce6
david93dev <david.flor@maisunifacisa.com.br>
2025-08-20 13:24:36 -0300

    chore: add shadsn and config init home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { ArrowBigLeft, ArrowRight } from "lucide-react";
 
-const home = () => {
+const Home = () => {
   return (
-    <div>home</div>
-  )
-}
+    <div className="bg-radial-[at_50%_25%] from-zinc-400 to-zinc-950 to-75% h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center px-4">
+        <h1 className="text-3xl text-center text-white font-bold">
+          Linguagem Interpretada
+        </h1>
+        <p className="text-sm max-w-2xl text-center text-white mt-4">
+          Projeto primeira entrega do curso de Linguagem Interpretada de David,
+          Isthanlley e João.
+        </p>
+        <div>
+          <Button className="mt-6">
+            Ver Alunos <ArrowRight />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default home
+export default Home;
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+// Em ESM, simulando __dirname
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
-   plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
