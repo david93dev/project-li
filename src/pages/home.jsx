d8ba101b8011879ad3b6ff119cf41c92fdeadcce
david93dev <david.flor@maisunifacisa.com.br>
2025-08-20 14:07:20 -0300

    feat: card

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -17,6 +17,84 @@ const Home = () => {
             Ver Alunos <ArrowRight />
           </Button>
         </div>
+        <div className="flex items-center w-[60%] justify-center my-4 border-t border-zinc-300 pt-4"></div>
+   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
+         <div className="bg-gradient-to-r from-zinc-700 to-zinc-500 rounded-2xl p-2 shadow-md">
+          <div className="flex items-center justify-center">
+            <div className="w-32 h-32 rounded-2xl overflow-hidden shadow-lg">
+              <img
+                src="https://www.pica-ai.com/_next/image/?url=https%3A%2F%2Fstatic.staticspic.com%2F_next%2Fstatic%2Fmedia%2FRectangle-2.1a85f280.jpg&w=640&q=75"
+                alt=""
+              />
+            </div>
+            <div className=" mx-8 border-r border-zinc-300 pt-19"></div>
+            <div className="flex flex-col items-start justify-center px-4">
+          
+              <h2 className="text-white font-semibold text-lg">
+                Carlos Santos
+              </h2>
+              <p className="text-cyan-400 font-semibold">
+                Full Stack Developer
+              </p>
+              <p className="text-gray-900 font-bold text-sm">Rio de Janeiro, RJ</p>
+              <p className="text-gray-900 font-bold text-sm">27 anos</p>
+              <div className="text-sm gap-2 flex items-center justify-center mt-2 text-gray-100 font-semibold cursor-pointer">
+                Ver Perfil <ArrowRight className="size-4" />
+              </div>
+            </div>
+          </div>
+        </div>
+              <div className="bg-gradient-to-r from-zinc-700 to-zinc-500 rounded-2xl p-2 shadow-md">
+          <div className="flex items-center justify-center">
+            <div className="w-32 h-32 rounded-2xl overflow-hidden shadow-lg">
+              <img
+                src="https://www.pica-ai.com/_next/image/?url=https%3A%2F%2Fstatic.staticspic.com%2F_next%2Fstatic%2Fmedia%2FRectangle-2.1a85f280.jpg&w=640&q=75"
+                alt=""
+              />
+            </div>
+            <div className=" mx-8 border-r border-zinc-300 pt-19"></div>
+            <div className="flex flex-col items-start justify-center px-4">
+          
+              <h2 className="text-white font-semibold text-lg">
+                Carlos Santos
+              </h2>
+              <p className="text-cyan-400 font-semibold">
+                Full Stack Developer
+              </p>
+              <p className="text-gray-900 font-bold text-sm">Rio de Janeiro, RJ</p>
+              <p className="text-gray-900 font-bold text-sm">27 anos</p>
+              <div className="text-sm gap-2 flex items-center justify-center mt-2 text-gray-100 font-semibold cursor-pointer">
+                Ver Perfil <ArrowRight className="size-4" />
+              </div>
+            </div>
+          </div>
+        </div>
+              <div className="bg-gradient-to-r from-zinc-700 to-zinc-500 rounded-2xl p-2 shadow-md">
+          <div className="flex items-center justify-center">
+            <div className="w-32 h-32 rounded-2xl overflow-hidden shadow-lg">
+              <img
+                src="https://www.pica-ai.com/_next/image/?url=https%3A%2F%2Fstatic.staticspic.com%2F_next%2Fstatic%2Fmedia%2FRectangle-2.1a85f280.jpg&w=640&q=75"
+                alt=""
+              />
+            </div>
+            <div className=" mx-8 border-r border-zinc-300 pt-19"></div>
+            <div className="flex flex-col items-start justify-center px-4">
+          
+              <h2 className="text-white font-semibold text-lg">
+                Carlos Santos
+              </h2>
+              <p className="text-cyan-400 font-semibold">
+                Full Stack Developer
+              </p>
+              <p className="text-gray-900 font-bold text-sm">Rio de Janeiro, RJ</p>
+              <p className="text-gray-900 font-bold text-sm">27 anos</p>
+              <div className="text-sm gap-2 flex items-center justify-center mt-2 text-gray-100 font-semibold cursor-pointer">
+                Ver Perfil <ArrowRight className="size-4" />
+              </div>
+            </div>
+          </div>
+        </div>
+   </div>
       </div>
     </div>
   );
